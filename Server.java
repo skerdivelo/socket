377416a77e6f3e2751e669b0249003ae3e4d6193
Skerdi <81383971+skerdivelo@users.noreply.github.com>
2023-10-18 09:08:23 +0000

    Socket Client-Server Server-Client

---
+++ Server.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.net.*;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        ServerSocket ss = new ServerSocket(4999);
+        Socket s = ss.accept(); // This is a blocking call
+
+        System.out.println("Client connected.");
+
+        InputStreamReader in = new InputStreamReader(s.getInputStream());
+        BufferedReader bf = new BufferedReader(in);
+
+        String str = bf.readLine();
+        System.out.println("Client: " + str);
+        
+        PrintWriter pr = new PrintWriter(s.getOutputStream());
+        pr.println("yessings");
+        pr.flush();
+    }
+}
